eeed1d0a4f928fc31afbd396f9073269de4f266d
alphago004 <sagarbhetuwal48@gmail.com>
2023-09-26 17:00:08 -0500

    changed UI

--- boredjoke/src/App.js
+++ boredjoke/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { MdSentimentVeryDissatisfied, MdInsertEmoticon } from 'react-icons/md'; // Importing Icons
 
 function App() {
     const [output, setOutput] = useState("");
@@ -18,20 +19,26 @@ function App() {
             let response = await fetch('http://localhost:3001/joke');
             let data = await response.json();
             if (data.setup && data.delivery) {
-                setOutput(data.setup + " " + data.delivery);
+                setOutput(data.setup + "\n\n" + "=> "+data.delivery);
             } else {
                 setOutput(data.joke || "Error fetching joke.");
             }
         } catch (error) {
-            setOutput("Error fetching joke.");
+            setOutput("Hold on!! Take a breath!");
         }
     }
 
     return (
         <div className="App">
-            <button onClick={getBoredActivity}>Get Bored Activity</button>
-            <button onClick={getJoke}>Get Joke</button>
             <textarea value={output} readOnly />
+            <div>
+                <button onClick={getBoredActivity}>
+                    <MdSentimentVeryDissatisfied /> Get Bored Activity
+                </button>
+                <button onClick={getJoke}>
+                    <MdInsertEmoticon /> Get Joke
+                </button>
+            </div>
         </div>
     );
 }
