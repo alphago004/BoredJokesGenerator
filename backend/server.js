84f7d89eaf914207162620b91a197c1b3e078971
alphago004 <sagarbhetuwal48@gmail.com>
2023-09-26 16:56:32 -0500

    updated UI

--- backend/server.js
+++ backend/server.js
@@ -17,12 +17,22 @@ app.get('/bored', async (req, res) => {
 app.get('/joke', async (req, res) => {
     try {
         const response = await axios.get('https://v2.jokeapi.dev/joke/Any');
-        res.json(response.data);
+        const jokeData = response.data;
+
+        // Extract the category and the joke content
+        const jokeInfo = {
+            category: jokeData.category,
+            setup: jokeData.setup,
+            delivery: jokeData.delivery
+        };
+        
+        res.json(jokeInfo);
+        
     } catch (error) {
-        res.status(500).json({ error: "Error fetching joke." });
+        res.status(500).json({ error: "Hold on!! Take a breath!" });
     }
 });
 
-app.listen(3001, () => {
+app.listen(3001, '0.0.0.0', () => {
     console.log('Server is up on port 3001');
-});
+  });
