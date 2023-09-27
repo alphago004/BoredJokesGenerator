8eaa665b6fbb4b3c75d60726a9bd49d99bcbe2d6
alphago004 <sagarbhetuwal48@gmail.com>
2023-09-26 22:03:12 -0500

    countries api added

--- boredjoke/src/App.js
+++ boredjoke/src/App.js
@@ -1,8 +1,13 @@
+import './App.css';
+
 import React, { useState } from 'react';
 import { MdSentimentVeryDissatisfied, MdInsertEmoticon } from 'react-icons/md'; // Importing Icons
 
 function App() {
     const [output, setOutput] = useState("");
+const [countries, setCountries] = useState([]);  // Ensure this is declared
+const [displayMode, setDisplayMode] = useState('');  // Ensure this is declared as well
+
 
     const getBoredActivity = async () => {
         try {
@@ -19,7 +24,7 @@ function App() {
             let response = await fetch('http://localhost:3001/joke');
             let data = await response.json();
             if (data.setup && data.delivery) {
-                setOutput(data.setup + "\n\n" + "=> "+data.delivery);
+                setOutput("Category: " +  data.category + "\n\n" + data.setup + "\n\n" + "=> "+data.delivery + "\n\n" );
             } else {
                 setOutput(data.joke || "Error fetching joke.");
             }
@@ -28,15 +33,42 @@ function App() {
         }
     }
 
+    const getCountryInfo = async () => {
+        try {
+            const response = await fetch('http://localhost:3001/country');
+            const data = await response.json();
+            setCountries(data);
+            setDisplayMode('country');
+        } catch (error) {
+            setOutput("Error fetching country info.");
+        }
+    }
+
     return (
         <div className="App">
-            <textarea value={output} readOnly />
+            {displayMode === 'country' ? (
+                <div>
+                    {countries.map((country, index) => (
+                        <div key={index}>
+                            <h3>{country.name}</h3>
+                            <p>Capital: {country.capital}</p>
+                            <p>Language: {country.language}</p>
+                            <img src={country.flag} className='small-flag' alt={`${country.name} flag`} />
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <textarea value={output} readOnly />
+            )}
             <div>
                 <button onClick={getBoredActivity}>
                     <MdSentimentVeryDissatisfied /> Get Bored Activity
                 </button>
                 <button onClick={getJoke}>
                     <MdInsertEmoticon /> Get Joke
+                </button>
+                <button onClick={getCountryInfo}>
+                    <MdInsertEmoticon /> Get Country Info
                 </button>
             </div>
         </div>
