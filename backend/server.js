febf7a8dca4abdfcf7bfda18a69e92522ae498fb
alphago004 <sagarbhetuwal48@gmail.com>
2023-09-26 22:03:41 -0500

    another api addded

--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,30 @@ app.get('/joke', async (req, res) => {
     }
 });
 
+
+app.get('/country', async (req, res) => {
+    try {
+        const response = await axios.get('https://restcountries.com/v3.1/all');
+        const countries = response.data;
+
+        // Extract required details for the first 10 countries
+        const firstTenCountriesInfo = countries.slice(0, 10).map(country => {
+            return {
+                name: country.name.common,
+                capital: country.capital ? country.capital[0] : 'N/A', // Some countries might not have a capital
+                language: Object.values(country.languages)[0],  // Extracting the first language, as countries can have multiple languages
+                flag: country.flags.svg 
+            };
+        });
+
+        res.json(firstTenCountriesInfo);
+        
+    } catch (error) {
+        res.status(500).json({ error: "Hold on!! Take a breath!" });
+    }
+});
+
+
 app.listen(3001, '0.0.0.0', () => {
     console.log('Server is up on port 3001');
   });
